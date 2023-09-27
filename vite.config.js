d585439d10577a3c4fe1dda943a92f88b2b9fe26
howtobefine <b4808697b@gmail.com>
2023-09-27 13:54:25 +0800

    sourcemap upload

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -7,11 +8,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), sentryVitePlugin({
+    org: "testorg-3df4e7207",
+    project: "vue0927"
+  })],
+
   base: process.env.NODE_ENV === 'production' ? '/plat-records/' : '/',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  build: {
+    sourcemap: true
   }
 })
